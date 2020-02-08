c82a3adb4d3def517b33e329d2c7fc592d802886
Trung Vuong Thien <vttrung12@gmail.com>
2020-02-08 01:30:08 -0500

    Add script to pull docker images

---
+++ scripts/setup_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a images=("citelab/alpine:latest"
+                   "citelab/consul-server:latest"
+                   "citelab/flask-server:latest"
+                   "citelab/glinux:latest"
+                   "citelab/haproxy:latest"
+                   "citelab/registrator:latest"
+                   "citelab/sfc-network-monitoring:latest"
+                   "citelab/sfc-rate-limiter:latest"
+                   "citelab/simplecloud:latest")
+
+for image in "${images[@]}"
+do
+    docker pull $image
+done
+
